0252924a45eefa405d66b45d696d68b93ab92229
Sergey Nikitin <nikitinserv@gmail.com>
2016-09-13 19:39:15 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        MyBig myBig = new MyBig(999);
+        myBig.addToIt(new MyBig(999999));
+        System.out.println(myBig);
     }
 }
